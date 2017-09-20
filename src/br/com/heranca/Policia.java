9adc09e7d20a6d128d66855015213d9229741fe6
jrks1998 <jonasr.k.s1998@gmail.com>
2017-09-20 02:54:49 -0300

    Modificação na classe Policia

--- src/br/com/heranca/Policia.java
+++ src/br/com/heranca/Policia.java
@@ -2,18 +2,6 @@ package br.com.heranca;
 
 public class Policia extends Cidadao{
 	
-	public String GetNome(String nomee) {
-		return nome = nomee;
-	}
-	
-	public String GetCPF(String cpfCidadao) {
-		return cpf = cpfCidadao;
-	}
-	
-	public boolean GetEstado() {
-		return bebeu;
-	}
-	
 	public boolean GetDecisao() {
 		if (bebeu == true) {
 			System.out.println("CE TA FUDIDO MEU PACERO!!");
